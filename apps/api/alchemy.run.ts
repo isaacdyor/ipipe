9c0d72dc0a99cdab798bcb7a8e77a210f2c980e2
Isaac Dyor <isaac@dyor.com>
2025-11-30 15:42:59 -0800

    alchemy effect example

---
+++ apps/api/alchemy.run.ts
@@ -0,0 +1,45 @@
+import { FetchHttpClient } from "@effect/platform";
+import { NodeContext } from "@effect/platform-node";
+import * as Alchemy from "alchemy-effect";
+import * as AWS from "alchemy-effect/aws";
+import * as CLI from "alchemy-effect/cli";
+import { Layer, Logger } from "effect";
+import * as Effect from "effect/Effect";
+import { Api, Consumer } from "./src/index.ts";
+
+// select your underlying platform
+const platform = Layer.mergeAll(
+  NodeContext.layer,
+  FetchHttpClient.layer,
+  Logger.pretty
+);
+
+// select your providers
+const providers = Layer.mergeAll(AWS.live);
+
+// override alchemy state store, CLI/reporting and dotAlchemy
+const alchemy = Layer.mergeAll(
+  Alchemy.State.localFs,
+  CLI.layer,
+  // optional
+  Alchemy.dotAlchemy
+);
+
+// define your app
+const app = Alchemy.app({ name: "my-app", stage: "dev" });
+
+const layers = Layer.provideMerge(
+  Layer.provideMerge(providers, alchemy),
+  Layer.mergeAll(platform, app)
+);
+
+await Alchemy.apply(Api, Consumer).pipe(
+  Effect.provide(layers),
+  Effect.tap((stack) =>
+    Effect.log({
+      url: stack?.Consumer.functionUrl,
+      queueUrl: stack?.Messages.queueUrl,
+    })
+  ),
+  Effect.runPromiseExit
+);
---
+++ apps/api/src/api.ts
@@ -0,0 +1,65 @@
+import { $, type } from "alchemy-effect";
+import * as DynamoDB from "alchemy-effect/aws/dynamodb";
+import * as Lambda from "alchemy-effect/aws/lambda";
+import * as SQS from "alchemy-effect/aws/sqs";
+import * as Effect from "effect/Effect";
+import * as Layer from "effect/Layer";
+import * as S from "effect/Schema";
+
+export interface User {
+  id: `USER#${string}`;
+  name: string;
+  age: number;
+}
+
+export interface CartItem {
+  id: `CART_ITEM#${string}`;
+  name: string;
+  productId: string;
+}
+
+export class SingleTable extends DynamoDB.Table("Users", {
+  partitionKey: "id",
+  sortKey: "name",
+  items: type<User | CartItem>,
+  attributes: {
+    id: S.String,
+    name: S.String,
+  },
+}) {}
+
+export class Api extends Lambda.serve("Api", {
+  fetch: Effect.fn(function* (event) {
+    const id = "USER#123";
+
+    const item = yield* DynamoDB.getItem({
+      table: SingleTable,
+      key: {
+        id,
+        name: "world",
+      },
+      projectionExpression: `${$.join(["id", "name"], ",")}, age`,
+    }).pipe(Effect.catchAll(() => Effect.succeed(undefined)));
+
+    return {
+      body: JSON.stringify(item?.Item),
+    };
+  }),
+})({
+  main: import.meta.filename,
+  // Infer instead of hard-code - always least-privilege
+  // TODO(sam): implement the compiler plugin
+  // bindings: $.infer(),
+  // OR: do it manually if you want to manually enforce a policy scope
+  bindings: $(
+    DynamoDB.GetItem(SingleTable, {
+      leadingKeys: $.anyOf("USER#123"),
+      attributes: $.anyOf("id", "name", "age"),
+    })
+  ),
+}) {}
+
+export default Api.handler.pipe(
+  Effect.provide(Layer.mergeAll(SQS.clientFromEnv(), DynamoDB.clientFromEnv())),
+  Lambda.toHandler
+);
